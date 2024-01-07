bc37ec138dbcca3659e2dbf2609ef3b190064d0a
montvlein <montvlein@gmail.com>
2024-01-07 16:57:20 -0300

    se agrego calendario por dia

--- app/page.js
+++ app/page.js
@@ -1,113 +1,288 @@
-import Image from 'next/image'
+'use client'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="flex h-full flex-col">
+      <CalendarHeader />
+      <div className="isolate flex flex-auto overflow-hidden bg-white divisor-x divisor-gray-900">
+        <AsideMenu />
+        <Calendar/>
       </div>
     </main>
   )
+}
+
+import { useEffect, useRef } from 'react'
+import CalendarHeader from '@components/Calendar/calendarHeader'
+import AsideMenu from '@/components/Aside'
+
+function Calendar() {
+  const container = useRef(null)
+  const containerNav = useRef(null)
+  const containerOffset = useRef(null)
+
+  useEffect(() => {
+    // Set the container scroll position based on the current time.
+    const currentMinute = new Date().getHours() * 60
+    container.current.scrollTop =
+      ((container.current.scrollHeight - containerNav.current.offsetHeight - containerOffset.current.offsetHeight) *
+        currentMinute) /
+      1440
+  }, [])
+
+  return (
+    <div ref={container} className="flex flex-auto flex-col overflow-auto">
+      <div
+        ref={containerNav}
+        className="sticky top-0 z-10 grid flex-none grid-cols-7 bg-white text-xs text-gray-500 shadow ring-1 ring-black ring-opacity-5 md:hidden"
+      >
+        <button type="button" className="flex flex-col items-center pb-1.5 pt-3">
+          <span>W</span>
+          {/* Default: "text-gray-900", Selected: "bg-gray-900 text-white", Today (Not Selected): "text-indigo-600", Today (Selected): "bg-indigo-600 text-white" */}
+          <span className="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-gray-900">
+            19
+          </span>
+        </button>
+        <button type="button" className="flex flex-col items-center pb-1.5 pt-3">
+          <span>T</span>
+          <span className="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-indigo-600">
+            20
+          </span>
+        </button>
+        <button type="button" className="flex flex-col items-center pb-1.5 pt-3">
+          <span>F</span>
+          <span className="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-gray-900">
+            21
+          </span>
+        </button>
+        <button type="button" className="flex flex-col items-center pb-1.5 pt-3">
+          <span>S</span>
+          <span className="mt-3 flex h-8 w-8 items-center justify-center rounded-full bg-gray-900 text-base font-semibold text-white">
+            22
+          </span>
+        </button>
+        <button type="button" className="flex flex-col items-center pb-1.5 pt-3">
+          <span>S</span>
+          <span className="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-gray-900">
+            23
+          </span>
+        </button>
+        <button type="button" className="flex flex-col items-center pb-1.5 pt-3">
+          <span>M</span>
+          <span className="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-gray-900">
+            24
+          </span>
+        </button>
+        <button type="button" className="flex flex-col items-center pb-1.5 pt-3">
+          <span>T</span>
+          <span className="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-gray-900">
+            25
+          </span>
+        </button>
+      </div>
+      <div className="flex w-full flex-auto">
+        <div className="w-14 flex-none bg-white ring-1 ring-gray-100" />
+        <div className="grid flex-auto grid-cols-1 grid-rows-1">
+          {/* Horizontal lines */}
+          <div
+            className="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100"
+            style={{ gridTemplateRows: 'repeat(48, minmax(3.5rem, 1fr))' }}
+          >
+            <div ref={containerOffset} className="row-end-1 h-7"></div>
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                12AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                1AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                2AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                3AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                4AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                5AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                6AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                7AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                8AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                9AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                10AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                11AM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                12PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                1PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                2PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                3PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                4PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                5PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                6PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                7PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                8PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                9PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                10PM
+              </div>
+            </div>
+            <div />
+            <div>
+              <div className="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+                11PM
+              </div>
+            </div>
+            <div />
+          </div>
+          <DayEvents />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function DayEvents() {
+  return(
+  <ol
+    className="col-start-1 col-end-2 row-start-1 grid grid-cols-1"
+    style={{ gridTemplateRows: '1.75rem repeat(288, minmax(0, 1fr)) auto' }}
+  >
+    <li className="relative mt-px flex" style={{ gridRow: '74 / span 12' }}>
+      <a
+        href="#"
+        className="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-blue-50 p-2 text-xs leading-5 hover:bg-blue-100"
+      >
+        <p className="order-1 font-semibold text-blue-700">Breakfast</p>
+        <p className="text-blue-500 group-hover:text-blue-700">
+          <time dateTime="2022-01-22T06:00">6:00 AM</time>
+        </p>
+      </a>
+    </li>
+    <li className="relative mt-px flex" style={{ gridRow: '92 / span 30' }}>
+      <a
+        href="#"
+        className="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-pink-50 p-2 text-xs leading-5 hover:bg-pink-100"
+      >
+        <p className="order-1 font-semibold text-pink-700">Flight to Paris</p>
+        <p className="order-1 text-pink-500 group-hover:text-pink-700">
+          John F. Kennedy International Airport
+        </p>
+        <p className="text-pink-500 group-hover:text-pink-700">
+          <time dateTime="2022-01-22T07:30">7:30 AM</time>
+        </p>
+      </a>
+    </li>
+    <li className="relative mt-px flex" style={{ gridRow: '134 / span 18' }}>
+      <a
+        href="#"
+        className="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-indigo-50 p-2 text-xs leading-5 hover:bg-indigo-100"
+      >
+        <p className="order-1 font-semibold text-indigo-700">Sightseeing</p>
+        <p className="order-1 text-indigo-500 group-hover:text-indigo-700">Eiffel Tower</p>
+        <p className="text-indigo-500 group-hover:text-indigo-700">
+          <time dateTime="2022-01-22T11:00">11:00 AM</time>
+        </p>
+      </a>
+    </li>
+  </ol>)
 }
