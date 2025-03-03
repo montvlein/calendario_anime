854fac53f87f1d660e75b6f02ea5c9f1b4a02cb0
montvlein <montvlein@gmail.com>
2025-03-03 11:30:46 -0300

    Se actualizo page para solo usar el elemento del calendario por semana

--- app/(page)/page.js
+++ app/(page)/page.js
@@ -1,51 +1,90 @@
 'use client'
 
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-
-import CalendarHeader from '@components/Header'
-import AsideMenu from '@components/Aside'
-import { useContextHook } from '../client/context/contextHook';
+import { useLatestAnimeList } from '@/app/client/hooks/getLastestAnimeList'
+import { useState } from 'react'
 
 export default function Home() {
+    const { animeList, loading, error } = useLatestAnimeList()
+    const [mostRecentAnime, setMostRecentAnime] = useState(0)
 
-  const {
-    selectedViewType
-  } =  useContextHook()
+    const resDays = (date, days) => {
+      return new Date(date.getTime() - (days * 24 * 60 * 60 * 1000));
+    }
 
-  return (
-    <main className="flex h-screen flex-col">
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <CalendarHeader />
-      <div className="isolate flex flex-auto overflow-hidden bg-white divide-x">
-        <AsideMenu />
-        <section className='flex-1 overflow-y-scroll'>
-          <Calendar calendarType={selectedViewType} />
-        </section>
-      </div>
-    </LocalizationProvider>
-    </main>
-  )
+    const calcWeekView = (lastWeekDay) => {
+      const weekDates = []
+      const arrayOf7num = Array.from({ length: 7 }, (v, i) => i)
+      for (let day = 7; day > 0; day--) {
+        const leftDay = arrayOf7num[day-1]
+        const weekDay = resDays(lastWeekDay, leftDay)
+        const animeOfDay = animeList.filter(anime => {
+            const animeDate = new Date(anime.releaseDate).toISOString().split('T')[0]
+            const weekDayDate = weekDay.toISOString().split('T')[0]
+            return animeDate === weekDayDate
+        })
+        const animeLen = animeOfDay.length
+        if (animeLen > mostRecentAnime) { setMostRecentAnime(animeLen) }
+        weekDates.push({date: weekDay, day: weekDay.getDay(), anime: animeOfDay})
+      }
+
+      return weekDates
+    }
+
+    const dateView = calcWeekView(new Date())
+
+    if (loading) return <div>Loading...</div>
+    if (error) return <div>Error: {error}</div>
+
+    return (
+        <main className="relative overflow-x-auto">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+                    <tr>
+                        {dateView.map((date, index) => (
+                            <th key={index} scope="col" className={`px-6 py-3 capitalize ${index%2 ? 'bg-gray-50 dark:bg-gray-800' : 'bg-gray-100 dark:bg-gray-700'}`}>
+                                <span className="flex gap-2 items-center">
+                                    {date.date.toLocaleDateString('es-ES', { weekday: 'short' })}
+                                    <span className={
+                                        new Date().getDay() === date.day ?
+                                            "flex h-8 w-8 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white" :
+                                            "items-center justify-center font-semibold"
+                                        } >{date.date.getDate()}</span>
+                                </span>
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    { Array.from({ length: mostRecentAnime }, (v, i) => (
+                        <tr key={i} className={`border-b border-gray-200 dark:border-gray-700 ${i%2 ? 'opacity-90' : ''}`}>
+                            {dateView.map((date, index) => (
+                                <td key={index} className={`text-center px-6 py-4 ${index%2 ? 'bg-gray-50 dark:bg-gray-800' : 'bg-gray-100 dark:bg-gray-700'}`}>
+                                    { date.anime[i] && <EventCard key={index} event={date.anime[i]} /> }
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
+        </main>
+    )
 }
 
-import DayCalendar from '@components/Calendar/day'
-import WeekCalendar from '@components/Calendar/week'
-import SeasonCalendar from '@components/Calendar/season'
-
-function Calendar({calendarType}) {
-
-  const {
-    calendarTypes
-  } =  useContextHook()
-
-  switch (calendarType) {
-    case calendarTypes.day:
-      return <DayCalendar/>
-    case calendarTypes.week:
-      return <WeekCalendar />
-    case calendarTypes.season:
-      return <SeasonCalendar />
-    default:
-      break;
-  }
+function EventCard({ event }) {
+    return (
+        <a
+            href={event.url}
+            className="group flex flex-col justify-center place-items-center text-xs leading-5 rounded-lg overflow-hidden relative"
+        >
+            <img src={event.cover} alt={event.title} className="w-full object-cover rounded-lg w-[200px] h-[140px]" />
+            <time dateTime={event.releaseDate} className="hidden text-xs">{event.releaseDate}</time>
+            <p className="absolute bottom-10 left-1/2 transform -translate-x-1/2 translate-y-full
+                        opacity-0 group-hover:opacity-100 transition-opacity duration-200
+                        bg-black/80 text-white text-xs px-2 py-1 rounded-md whitespace-nowrap
+                        pointer-events-none z-10">
+                {event.title} - <span className="text-gray-300">cap. {event.chapter}</span>
+            </p>
+        </a>
+    )
 }
---
+++ app/(page)/page_old_version.js
@@ -0,0 +1,51 @@
+'use client'
+
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+
+import CalendarHeader from '@components/Header'
+import AsideMenu from '@components/Aside'
+import { useContextHook } from '../client/context/contextHook';
+
+export default function Home() {
+
+  const {
+    selectedViewType
+  } =  useContextHook()
+
+  return (
+    <main className="flex h-screen flex-col">
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <CalendarHeader />
+      <div className="isolate flex flex-auto overflow-hidden bg-white divide-x">
+        <AsideMenu />
+        <section className='flex-1 overflow-y-scroll'>
+          <Calendar calendarType={selectedViewType} />
+        </section>
+      </div>
+    </LocalizationProvider>
+    </main>
+  )
+}
+
+import DayCalendar from '@components/Calendar/day'
+import WeekCalendar from '@components/Calendar/week'
+import SeasonCalendar from '@components/Calendar/season'
+
+function Calendar({calendarType}) {
+
+  const {
+    calendarTypes
+  } =  useContextHook()
+
+  switch (calendarType) {
+    case calendarTypes.day:
+      return <DayCalendar/>
+    case calendarTypes.week:
+      return <WeekCalendar />
+    case calendarTypes.season:
+      return <SeasonCalendar />
+    default:
+      break;
+  }
+}
