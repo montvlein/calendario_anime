2e5819cf4e550a2a1dc883617e8a950f37d8b7f6
montvlein <montvlein@gmail.com>
2025-03-03 11:50:36 -0300

    se agrego efecto

--- app/(page)/page.js
+++ app/(page)/page.js
@@ -36,13 +36,13 @@ export default function Home() {
     if (error) return <div>Error: {error}</div>
 
     return (
-        <main className="relative overflow-x-auto">
+        <main className="">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
-                    <tr>
+                    <tr className='sticky top-0 z-10'>
                         {dateView.map((date, index) => (
                             <th key={index} scope="col" className={`px-6 py-3 capitalize ${index%2 ? 'bg-gray-50 dark:bg-gray-800' : 'bg-gray-100 dark:bg-gray-700'}`}>
-                                <span className="flex gap-2 items-center">
+                                <span className="flex gap-2 items-center justify-center">
                                     {date.date.toLocaleDateString('es-ES', { weekday: 'short' })}
                                     <span className={
                                         new Date().getDay() === date.day ?
@@ -56,7 +56,7 @@ export default function Home() {
                 </thead>
                 <tbody>
                     { Array.from({ length: mostRecentAnime }, (v, i) => (
-                        <tr key={i} className={`border-b border-gray-200 dark:border-gray-700 ${i%2 ? 'opacity-90' : ''}`}>
+                        <tr key={i} className={`border-b border-gray-200 dark:border-gray-700 ${i%2 ? 'opacity-95' : ''}`}>
                             {dateView.map((date, index) => (
                                 <td key={index} className={`text-center px-6 py-4 ${index%2 ? 'bg-gray-50 dark:bg-gray-800' : 'bg-gray-100 dark:bg-gray-700'}`}>
                                     { date.anime[i] && <EventCard key={index} event={date.anime[i]} /> }
@@ -75,15 +75,15 @@ function EventCard({ event }) {
     return (
         <a
             href={event.url}
-            className="group flex flex-col justify-center place-items-center text-xs leading-5 rounded-lg overflow-hidden relative"
+            className="group flex flex-col justify-center place-items-center text-xs leading-5 rounded-lg relative"
         >
-            <img src={event.cover} alt={event.title} className="w-full object-cover rounded-lg w-[200px] h-[140px]" />
+            <img src={event.cover} alt={event.title} className="w-full object-cover rounded-lg w-[200px] h-[140px] transition-transform duration-300 group-hover:scale-125" />
             <time dateTime={event.releaseDate} className="hidden text-xs">{event.releaseDate}</time>
             <p className="absolute bottom-10 left-1/2 transform -translate-x-1/2 translate-y-full
                         opacity-0 group-hover:opacity-100 transition-opacity duration-200
-                        bg-black/80 text-white text-xs px-2 py-1 rounded-md whitespace-nowrap
+                        bg-black/80 text-white text-xs px-2 py-1 rounded-md
                         pointer-events-none z-10">
-                {event.title} - <span className="text-gray-300">cap. {event.chapter}</span>
+                <span className="font-semibold line-clamp-3">{event.title}</span> <span className="text-gray-300">cap. {event.chapter}</span>
             </p>
         </a>
     )
